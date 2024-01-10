40fc85977087c19b77c117afd6c18c0e6ec30581
anarehder <anapaula.rehder@gmail.com>
2024-01-10 13:05:24 -0300

    fix: responsividade header

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -35,4 +35,11 @@ const Header = styled.div`
     img {
         width: 100px;
     }
+    @media (max-width: 500px) {
+        flex-wrap: wrap;
+        justify-content: center;
+        img {
+            width: 0;
+        }
+    }
 `
