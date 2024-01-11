d08291de1c94e64e6ed36f32ecab434b2786a1d8
anarehder <anapaula.rehder@gmail.com>
2024-01-11 09:29:11 -0300

    admin page exibir dados

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -12,8 +12,8 @@ function AdminPage(){
     const [user, setUser] = useContext(UserContext);
     const [form, setForm] = useState({ month: "", employeeId: 0});
     const [data, setData] = useState([]);
+    const [hours, setHours] = useState([]);
     const [employees, setEmployees] = useState([]);
-    const [bank, setBank] = useState({totalHours:'16:30',previousMonthBalance:'+ 10:15', bankHours:'- 16:30'});
     const [employeeInfo, setEmployeeInfo] = useState({ month: "", name: ""});
 
     console.log(data);
@@ -57,12 +57,14 @@ function AdminPage(){
                 const response = await apiService.getUserReport(user.token, form);
                 if (response.status === 200) {
                     setData(response.data);
+                    setHours(response.data.hourControls);
                 }
             } catch (error) {
                 console.log(error);
-            } finally {
-                setForm({ month: '', employeeId: '' });
             }
+            // } finally {
+            //     setForm({ month: '', employeeId: '' });
+            // }
         }
     };
 
@@ -95,16 +97,90 @@ function AdminPage(){
                     <p>Exibir dados</p>
                     <GoArrowRight size={24} />
                 </button>   
-            </MainContainer>    
-            {(form.employeeId && form.month !== "") ?
-                <div>
-                    <h2>Detalhes do Funcionário:</h2>
-                    <p>ID: {form.employeeId}</p>
-                </div>
+            </MainContainer>
+            {data.length !== 0 ?
+                <DataArea>
+                    <TableHeader>
+                        <h1>Data</h1>
+                        <h1>Entrada</h1>
+                        <h1>Pausa</h1>
+                        <h1>Retorno</h1>
+                        <h1>Saída</h1>
+                        <h1>Horas/Dia</h1>
+                    </TableHeader>
+                    <Daily>
+                        {hours.map((d, i) => (
+                            <div key={i}>
+                                <h2>
+                                    {d.day.slice(0, 10)}
+                                </h2>
+                                <h2>
+                                    {d.entry_time ? d.entry_time.slice(11, 16) : "-"}
+                                </h2>
+                                <h2>
+                                    {d.pause_time ? d.pause_time.slice(11, 16) : "-"}
+                                </h2>
+                                <h2>
+                                    {d.return_time ? d.return_time.slice(11, 16) : "-"}
+                                </h2>
+                                <h2>
+                                    {d.exit_time ? d.exit_time.slice(11, 16) : "-"}
+                                </h2>
+                                <h2>
+                                    {d.totalWorkedByDay.slice(11, 16)}
+                                </h2>
+                            </div>
+                        ))}
+                    </Daily>
+                    <TableFooter>
+                        <div>
+                            <h1>
+                                Total Horas no Mês
+                            </h1>
+                            <h2>
+                                {data.bankHours.workedHoursByMonth}
+                            </h2>
+                        </div>
+                        <div>
+                            <h1>
+                                Saldo Horas no Mês
+
+                            </h1>
+
+                            <StyledParagraph color={data.bankHours.totalHoursByMonth.slice(0, 1)}>
+                                {data.bankHours.totalHoursByMonth}
+                            </StyledParagraph>
+
+                        </div>
+                        <div>
+                            <h1>
+                                Saldo Mês Anterior
+                            </h1>
+                            <StyledParagraph color={data.bankBalanceLastMonth.hoursBankBalance.slice(0, 1)}>
+                                {data.bankBalanceLastMonth.hoursBankBalance}
+                            </StyledParagraph>
+                        </div>
+                        <div>
+                            <h1>
+                                Banco de Horas
+                            </h1>
+                            <StyledParagraph color={data.bankHours.hoursBankBalance.slice(0, 1)}>
+                                {data.bankHours.hoursBankBalance}
+                            </StyledParagraph>
+                        </div>
+
+                    </TableFooter>
+                    <h3>
+                        *o saldo do mês é calculado considerando as 176 horas de cada mês que devem ser cumpridas ( Saldo Mês = Total Horas no Mês - 176 ).
+                    </h3>
+                    <h3>
+                        *o banco de horas considera o saldo do mês anterios e o saldo do mês atual ( Banco de Horas = Saldo Mês Atual + Saldo Mês Anterior ).
+                    </h3>
+                </DataArea>
                 :
                 <AlertView> Selecione os dados para visualizar </AlertView>
             }
-            {data.length !== 0 && <ExportToExcel name={employeeInfo.name} month={employeeInfo.month} data={data} bank={bank} />}
+            {data !== 0 && <ExportToExcel name={employeeInfo.name} month={employeeInfo.month} data={data} />}
             <Logout />
             <ReturnComponent name={user.name} />
         </PageContainer>
@@ -114,17 +190,20 @@ function AdminPage(){
 export default AdminPage;
 
 const PageContainer = styled.div`
-    width: 100%;
-    flex-direction: column;
-    align-items: center;
-    gap: 30px;
+width: 100%;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+gap: 20px;
+margin-bottom: 30px;
 `
 
 const MainContainer = styled.form`
     width: 70%;
     display:flex;
-    justify-content: space-between;
+    justify-content: center;
     align-items: center;
+    margin-bottom: 30px;
     button {
         height: 70px;
     }
@@ -150,4 +229,62 @@ const AlertView = styled.h1`
     background-color: #F0F5F9;
     padding: 15px;
     border-radius: 24px;
+`
+
+
+const DataArea = styled.div`
+    flex-direction: column;
+    justify-content: center;
+    border-radius: 24px;
+    background-color:#F0F5F9;
+    div {
+        margin: 10px 0;
+    }
+    h2 {
+        font-weight: 400;
+        font-size: 25px;
+        width: 125px;
+    }
+    h1 {
+        width: 125px;
+    }
+    h3 {
+        text-align: center;
+        margin: 5px 0;
+    }
+`
+
+const TableHeader = styled.div`
+    margin: 0 10px;
+    padding: 10px;
+    border-bottom: 2px solid #E6E6E6;
+    gap: 25px;
+    text-align: center;
+`
+
+const Daily = styled.div`
+    flex-direction: column;
+    justify-content: center;
+    margin: 0;
+    padding: 10px;
+    gap: 25px;
+    text-align: center;
+`
+
+const TableFooter = styled.div`
+    flex-wrap: wrap;
+    justify-content: center;
+    margin: 0 10px;
+    padding: 10px;
+    border-top: 2px solid #E6E6E6;
+    div {
+        padding: 0 25px;
+        text-align: center;
+        align-items: center;
+        gap: 20px;
+    }
+`
+
+const StyledParagraph = styled.h2`
+    color: ${(props) => (props.color === '+' ? '#1C8E09' : props.color === '-' ? '#C91313' : '#021121')}; 
 `
