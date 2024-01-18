6ea7aa6aac030533cd2a16ce6ec12be65ac239a2
anarehder <anapaula.rehder@gmail.com>
2024-01-18 16:48:42 -0300

    fix: api service url

--- src/services/apiService.jsx
+++ src/services/apiService.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_REACT_APP_API_URL;
+const BASE_URL = 'http://localhost:4000';
 
 function signIn(body) {
     return axios.post(`${BASE_URL}/login`, body);
